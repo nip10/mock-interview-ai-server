a78531fee2516ef7fa28c220e1bc652384c7131f
nip10 <diogocardoso92@outlook.com>
2024-10-06 17:06:00 +0100

    add build and deploy action

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import { RealtimeRelay } from "./lib/relay";
 import dotenv from "dotenv";
 dotenv.config({ override: true });
 
-// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const AZURE_AI_KEY = process.env.AZURE_AI_KEY;
+const AZURE_OPENAI_API_KEY = process.env.AZURE_OPENAI_API_KEY;
 
 const PORT = Number.parseInt(process.env.PORT) || 8081;
 
-const relay = new RealtimeRelay(AZURE_AI_KEY);
+const relay = new RealtimeRelay(AZURE_OPENAI_API_KEY);
 relay.listen(PORT);
